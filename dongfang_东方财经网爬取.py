e764b3d2c4b3e439f28ac1e71ca9d44cd67983f1
boris <boris.chen.czy@gmail.com>
2017-04-02 23:14:24 +0800

    finnaly

--- dongfang_东方财经网爬取.py
+++ dongfang_东方财经网爬取.py
@@ -5,34 +5,35 @@ r = s.get('http://www.baidu.com')
 base_url = 'http://guba.eastmoney.com/default_<num>.html'
 cnt = 0
 
-def get_tag(tag):
-    content = str(tag)
-    formula = '^(<.*?>)'
-    pattern = re.compile(formula)
-    tag = re.findall(pattern, content)[0]
-    return tag
-
+df = pd.DataFrame(pandas_head)
+# print(df)
+# quit()
 for i in range(1, 11):
     url = base_url.replace('<num>', str(i))
     r = s.get(url)
     soup = BeautifulSoup(r.text, 'lxml')
     spans = soup.find_all('span', class_ = 'sub')
+    cnt = 0
+    total = len(spans)
     for span in spans:
         cnt += 1
         if span.a:
             inner_url = 'http://guba.eastmoney.com' + span.find_all('a')[1].get('href')
             r = s.get(inner_url)
-            with open('./东方财经网/{}.html'.format(cnt), 'wb') as f:
-                f.write(r.text.encode('utf-8'))
             soup = BeautifulSoup(r.text, 'lxml')
             div = soup.find_all('div', class_ = 'stockcodec')[0]
             content = div.get_text()
             content_tag = get_tag(div)
-            parent_tags = []
-            parent = div
-            for i in range(5):
-                parent = parent.parent
-                parent_tag = get_tag(parent)
-                parent_tags.append(parent_tag)
-            
-            
+            content_parent_tags = find_parent(div)
+            author_div = soup.find(id = 'zwconttbn')
+            author = author_div.get_text()
+            author_tag = get_tag(author_div)
+            author_parent_tags = find_parent(author_div)
+            df = add_row(df, r.text, content, content_tag, *content_parent_tags, author, author_tag, *author_parent_tags)
+            # print(df)
+            # quit()
+            print('{}/{}'.format(cnt, total))
+
+df.to_csv('./东方财经.csv')
+
+
--- head.py
+++ head.py
@@ -1,3 +1,56 @@
 import requests
 import re
 from bs4 import BeautifulSoup
+import pandas as pd
+import numpy as np
+from collections import Iterator
+
+pandas_head = {
+    'HTML file': ['abs'],
+    'author': 1,
+    'author tag': 1,
+    'author parent1 tag': 1,
+    'author parent2 tag': 1,
+    'author parent3 tag': 1,
+    'author parent4 tag': 1,
+    'author parent5 tag': 1,
+    'content': 1,
+    'content tag': 1,
+    'content parent1 tag': 1,
+    'content parent2 tag': 1,
+    'content parent3 tag': 1,
+    'content parent4 tag': 1,
+    'content parent5 tag': 1,
+}
+
+index = sorted(pandas_head.keys())
+
+def get_tag(tag):
+    content = str(tag)
+    formula = '^(<.*?>)'
+    pattern = re.compile(formula)
+    tag = re.findall(pattern, content)[0]
+    return tag
+
+def find_parent(parent):
+    parent_tags = []
+    for i in range(5):
+        parent = parent.parent
+        parent_tag = get_tag(parent)
+        parent_tags.append(parent_tag)
+    return (parent_tags)
+
+def add_row(origin_dataframe, *datas):
+    all  = []
+    # print(origin_dataframe)
+    # quit()
+    for data in datas:
+            all.append(data)
+    series = pd.Series(all, index = index)
+    # print(series)
+    # s2 = pd.Series(['X0', 'X1', 'X2', 'X3'], index=['A', 'B', 'C', 'D'])
+    # print(s2)
+    # print(origin_dataframe)
+    # print(origin_dataframe.append(series, ignore_index = True))
+    # quit()
+    return origin_dataframe.append(series, ignore_index = True)
